ed8ba71319963eea268a43207dec805da2887bcc
Rick Gingrich <rick.gingrich@amprobotics.com>
2024-10-09 21:45:06 -0600

    default mage file

--- Magefile.go
+++ Magefile.go
@@ -4,51 +4,8 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	// mage:import
-
-	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
+	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
-// Default configures the default target.
-var Default = Build
-
-// Build builds the binaries.
-func Build() error {
-	return sh.RunV("go", "build", "./...")
-}
-
-// Test runs backend tests.
-func Test() error {
-	return sh.RunV("go", "test", "./...")
-}
-
-// Coverage runs backend tests and creates a coverage report.
-func Coverage() error {
-	fmt.Println("Running backend tests with coverage...")
-	if err := sh.Run("go", "test", "./...", "-coverprofile=coverage.out"); err != nil {
-		return err
-	}
-	return sh.Run("go", "tool", "cover", "-html=coverage.out", "-o", "coverage.html")
-}
-
-func Lint() error {
-	if err := sh.RunV("golangci-lint", "run", "./..."); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Check runs all checks (tests, coverage, and linting).
-func Check() {
-	mg.SerialDeps(Test, Coverage, Lint)
-}
-
-// Clean removes build artifacts.
-func Clean() error {
-	fmt.Println("Cleaning...")
-	return os.RemoveAll("dist")
-}
+var Default = build.BuildAll
